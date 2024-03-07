1318223e4b5aa798bd72878a41b6b762166c1790
Salman-sajib <sfsajib@gmail.com>
2024-03-08 00:04:38 +0600

    initial commit finished the project

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,35 @@
+import './card.css';
+
+function Card() {
+  return (
+    <div className='card-container'>
+      <div className='project-image'>
+        <img src='../../../src/assets/project1.png' alt='project1 image' />
+      </div>
+      <div className='project-detail'>
+        <h3 className='project-name'>GSAP Layout Animation</h3>
+        <p className='project-description'>
+          By clicking the nav links the underline jumps with a subtle animation
+          and the card&apos;s will expand and reveal the description and fold it
+          by clicking with a neat animation.
+        </p>
+        <div className='technology-used'>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>JAVASCRIPT</p>
+          <p>GSAP</p>
+        </div>
+        <div className='project-buttons'>
+          <button>
+            <a href='#'>Visit</a>
+          </button>
+          <button>
+            <a href='#'>Code</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
