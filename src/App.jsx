8e042247c653cd4133a69c5770740b3f6747c061
Salman-sajib <sfsajib@gmail.com>
2024-03-08 23:32:42 +0600

    deleted the card 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import Card from './components/card/Card';
-import Card2 from './components/card2/Card2';
 
 function App() {
   return (
     <div className='app'>
       <Card />
-      {/* <Card2 /> */}
     </div>
   );
 }
