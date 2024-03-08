a7aea6fa8c35300a8719b4c93a884c17d601ec19
Salman-sajib <sfsajib@gmail.com>
2024-03-08 20:35:47 +0600

    changed the colors

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,29 +4,31 @@ import myImage from '../../assets/project1.png';
 function Card() {
   return (
     <div className='card-container'>
-      <div className='project-image'>
-        <img src={myImage} alt='project image' />
-      </div>
-      <div className='project-detail'>
-        <h3 className='project-name'>GSAP Layout Animation</h3>
-        <p className='project-description'>
-          By clicking the nav links the underline jumps with a subtle animation
-          and the card&apos;s will expand and reveal the description and fold it
-          by clicking with a neat animation.
-        </p>
-        <div className='technology-used'>
-          <p>HTML</p>
-          <p>CSS</p>
-          <p>JAVASCRIPT</p>
-          <p>GSAP</p>
+      <div className='card'>
+        <div className='project-image'>
+          <img src={myImage} alt='project image' />
         </div>
-        <div className='project-buttons'>
-          <button>
-            <a href='#'>Visit</a>
-          </button>
-          <button>
-            <a href='#'>Code</a>
-          </button>
+        <div className='project-detail'>
+          <h3 className='project-name'>GSAP Layout Animation</h3>
+          <p className='project-description'>
+            By clicking the nav links the underline jumps with a subtle
+            animation and the card&apos;s will expand and reveal the description
+            and fold it by clicking with a neat animation.
+          </p>
+          <div className='technology-used'>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JAVASCRIPT</p>
+            <p>GSAP</p>
+          </div>
+          <div className='project-buttons'>
+            <button>
+              <a href='#'>Visit</a>
+            </button>
+            <button>
+              <a href='#'>Code</a>
+            </button>
+          </div>
         </div>
       </div>
     </div>
