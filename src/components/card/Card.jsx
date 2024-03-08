39704110348b578579b7445d5e48f5a6dcdf0022
Salman-sajib <sfsajib@gmail.com>
2024-03-09 00:10:01 +0600

    added an icon to the buttons

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -24,10 +24,30 @@ function Card() {
           <div className='learn-more'>
             <ul>
               <li>
-                <a href=''>Visit</a>
+                <a href=''>
+                  Visit{' '}
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='24'
+                    viewBox='0 -960 960 960'
+                    width='24'
+                  >
+                    <path d='m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z' />
+                  </svg>
+                </a>
               </li>
               <li>
-                <a href=''>Code</a>
+                <a href=''>
+                  Code{' '}
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='24'
+                    viewBox='0 -960 960 960'
+                    width='24'
+                  >
+                    <path d='m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z' />
+                  </svg>
+                </a>
               </li>
             </ul>
           </div>
